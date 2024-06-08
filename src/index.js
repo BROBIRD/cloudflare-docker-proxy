20bc22d8b8b7f92330455dd5aaa388c25b7d5ffa
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2024-06-08 10:22:30 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.zhizhang.pw": "https://registry-1.docker.io",
+  "quay.zhizhang.pw": "https://quay.io",
+  "gcr.zhizhang.pw": "https://gcr.io",
+  "k8s-gcr.zhizhang.pw": "https://k8s.gcr.io",
+  "k8s.zhizhang.pw": "https://registry.k8s.io",
+  "ghcr.zhizhang.pw": "https://ghcr.io",
+  "cloudsmith.zhizhang.pw": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
